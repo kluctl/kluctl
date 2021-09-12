af4ad5c73d6520d3703f0ef614248a2d2dc28eaf
Alexander Block <ablock84@gmail.com>
2021-09-12 16:45:27 +0200

    perf: Paralellize deletion of objects

--- kluctl/utils/k8s_delete_utils.py
+++ kluctl/utils/k8s_delete_utils.py
@@ -4,6 +4,7 @@ from kluctl.utils.dict_utils import get_dict_value
 from kluctl.utils.k8s_object_utils import get_included_objects, get_label_from_object, get_object_ref, \
     get_long_object_name_from_ref, split_api_version, get_filtered_api_resources, \
     remove_api_version_from_ref, remove_namespace_from_ref_if_needed
+from kluctl.utils.utils import MyThreadPoolExecutor
 
 logger = logging.getLogger(__name__)
 
@@ -82,9 +83,31 @@ def find_objects_for_delete(k8s_cluster, labels, inclusion, excluded_objects):
     return ret
 
 def delete_objects(k8s_cluster, object_refs, do_wait):
-    for ref in object_refs:
-        try:
-            k8s_cluster.delete_single_object(ref, do_wait)
-            logger.info('Deleted object %s' % get_long_object_name_from_ref(ref))
-        except Exception as e:
-            logger.error('Failed to delete object %s. Error=%s' % (get_long_object_name_from_ref(ref), k8s_cluster.get_status_message(e)))
+    namespaces = set(x for x in object_refs if x.kind == "Namespace")
+    namespace_names = set(x.name for x in namespaces)
+
+    def do_delete_object(ref):
+        logger.info('Deleting %s' % get_long_object_name_from_ref(ref))
+        k8s_cluster.delete_single_object(ref, do_wait)
+
+
+    with MyThreadPoolExecutor(max_workers=8) as executor:
+        futures = []
+        for ref in namespaces:
+            f = executor.submit(do_delete_object, ref)
+            futures.append(f)
+
+        for ref in object_refs:
+            if ref in namespaces:
+                continue
+            if ref.namespace in namespace_names:
+                continue
+
+            f = executor.submit(do_delete_object, ref)
+            futures.append(f)
+
+        for f in futures:
+            try:
+                f.result()
+            except Exception as e:
+                logger.error('Failed to delete %s. Error=%s' % (get_long_object_name_from_ref(ref), k8s_cluster.get_status_message(e)))
