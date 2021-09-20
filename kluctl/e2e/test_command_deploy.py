3b474a2d961b01136ba05f7fe3737789b69727b3
Alexander Block <ablock84@gmail.com>
2021-09-20 11:42:52 +0200

    test: Also test --dry-run in deploy command

--- kluctl/e2e/test_command_deploy.py
+++ kluctl/e2e/test_command_deploy.py
@@ -1,6 +1,6 @@
 from pytest_kind import KindCluster
 
-from kluctl.e2e.conftest import assert_resource_exists, recreate_namespace
+from kluctl.e2e.conftest import assert_resource_exists, recreate_namespace, assert_resource_not_exists
 from kluctl.e2e.kluctl_test_project import KluctlTestProject
 from kluctl.e2e.kluctl_test_project_helpers import add_configmap_deployment
 
@@ -14,5 +14,10 @@ def test_command_deploy_simple(module_kind_cluster: KindCluster):
 
         add_configmap_deployment(p, "cm", "cm", namespace="simple")
         p.kluctl("deploy", "--yes", "-t", "test")
-
         assert_resource_exists(module_kind_cluster, "simple", "ConfigMap/cm")
+
+        add_configmap_deployment(p, "cm2", "cm2", namespace="simple")
+        p.kluctl("deploy", "--yes", "-t", "test", "--dry-run")
+        assert_resource_not_exists(module_kind_cluster, "simple", "ConfigMap/cm2")
+        p.kluctl("deploy", "--yes", "-t", "test")
+        assert_resource_exists(module_kind_cluster, "simple", "ConfigMap/cm2")
