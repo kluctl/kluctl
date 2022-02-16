64efb7aae011df4ef5cead747631b5492212d98b
Alexander Block <ablock84@gmail.com>
2022-02-16 15:26:29 +0100

    Implement new-version check

--- cmd/kluctl/root.go
+++ cmd/kluctl/root.go
@@ -16,10 +16,76 @@ limitations under the License.
 package main
 
 import (
+	"github.com/codablock/kluctl/pkg/utils"
+	"github.com/codablock/kluctl/pkg/utils/uo"
+	"github.com/codablock/kluctl/pkg/version"
+	"github.com/codablock/kluctl/pkg/yaml"
+	log "github.com/sirupsen/logrus"
+	"net/http"
 	"os"
+	"path"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
+
+var noUpdateCheck = false
+
+const latestReleaseUrl = "https://api.github.com/repos/codablock/kluctl/releases/latest"
+
+
+type VersionCheckState struct {
+	LastVersionCheck time.Time `yaml:"lastVersionCheck"`
+}
+
+func checkNewVersion() {
+	if noUpdateCheck {
+		return
+	}
+	if version.Version == "0.0.0" {
+		return
+	}
+
+	versionCheckPath := path.Join(utils.GetTmpBaseDir(), "version_check.yml")
+	var versionCheckState VersionCheckState
+	err := yaml.ReadYamlFile(versionCheckPath, &versionCheckState)
+	if err == nil {
+		if time.Now().Sub(versionCheckState.LastVersionCheck) < time.Hour {
+			return
+		}
+	}
+
+	versionCheckState.LastVersionCheck = time.Now()
+	_ = yaml.WriteYamlFile(versionCheckPath, &versionCheckState)
+
+	log.Debugf("Checking for new kluctl version")
+
+	r, err := http.Get(latestReleaseUrl)
+	if err != nil {
+		return
+	}
+	defer r.Body.Close()
+
+	var release uo.UnstructuredObject
+	err = yaml.ReadYamlStream(r.Body, &release)
+	if err != nil {
+		return
+	}
+
+	latestVersionStr, ok, _ := release.GetNestedString("tag_name")
+	if !ok {
+		return
+	}
+	if strings.HasPrefix(latestVersionStr, "v") {
+		latestVersionStr = latestVersionStr[1:]
+	}
+	latestVersion := utils.LooseVersion(latestVersionStr)
+	localVersion := utils.LooseVersion(version.Version)
+	if localVersion.Less(latestVersion, true) {
+		log.Warningf("You are using an outdated version (%v) of kluctl. You should update soon to version %v", localVersion, latestVersion)
+	}
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -27,6 +93,10 @@ var rootCmd = &cobra.Command{
 	Short: "Deploy and manage complex deployments on Kubernetes",
 	Long: `The missing glue to put together large Kubernetes deployments,
 composed of multiple smaller parts (Helm/Kustomize/...) in a manageable and unified way.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		checkNewVersion()
+		return nil
+	},
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -43,4 +113,6 @@ func Execute() {
 
 func init() {
 	rootCmd.SilenceUsage = true
+
+	rootCmd.Flags().BoolVar(&noUpdateCheck, "no-update-check", false, "Disable update check on startup")
 }
