21d0fe5ed0f1c592c8dbb44a8f3fbf93f20cb254
Alexander Block <ablock84@gmail.com>
2022-06-08 14:49:14 +0200

    fix: More tracing in ListAuthProvider and GitEnvAuthProvider

--- pkg/git/auth/env_auth_provider.go
+++ pkg/git/auth/env_auth_provider.go
@@ -26,11 +26,14 @@ func (a *GitEnvAuthProvider) BuildAuth(ctx context.Context, gitUrl git_url.GitUr
 		}
 
 		ssh_key_path, _ := m["SSH_KEY"]
+
+		status.Trace(ctx, "GitEnvAuthProvider: adding entry host=%s, pathPrefix=%s, username=%s, ssh_key=%s", e.Host, e.PathPrefix, e.Username, ssh_key_path)
+
 		if ssh_key_path != "" {
 			ssh_key_path = utils.ExpandPath(ssh_key_path)
 			b, err := ioutil.ReadFile(ssh_key_path)
 			if err != nil {
-				status.Trace(ctx, "Failed to read key %s: %v", ssh_key_path, err)
+				status.Trace(ctx, "GitEnvAuthProvider: failed to read key %s: %v", ssh_key_path, err)
 			} else {
 				e.SshKey = b
 			}
@@ -40,7 +43,7 @@ func (a *GitEnvAuthProvider) BuildAuth(ctx context.Context, gitUrl git_url.GitUr
 			ca_bundle_path = utils.ExpandPath(ca_bundle_path)
 			b, err := ioutil.ReadFile(ca_bundle_path)
 			if err != nil {
-				status.Trace(ctx, "Failed to read ca bundle %s: %v", ca_bundle_path, err)
+				status.Trace(ctx, "GitEnvAuthProvider: failed to read ca bundle %s: %v", ca_bundle_path, err)
 			} else {
 				e.CABundle = b
 			}
