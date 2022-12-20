07407b8ab90693be05ddbb66cd3d76389ff60c63
Alexander Block <ablock84@gmail.com>
2022-12-20 16:46:13 +0100

    fix: Also obfuscate secrets when data/stringData was empty before

--- e2e/obfuscate_test.go
+++ e2e/obfuscate_test.go
@@ -25,16 +25,25 @@ func TestObfuscateSecrets(t *testing.T) {
 		name:      "secret",
 		namespace: p.TestSlug(),
 	}, false)
+	addSecretDeployment(p, "secret2", nil, resourceOpts{
+		name:      "secret2",
+		namespace: p.TestSlug(),
+	}, false)
+	addSecretDeployment(p, "secret3", nil, resourceOpts{
+		name:      "secret3",
+		namespace: p.TestSlug(),
+	}, false)
+
 	stdout, _ := p.KluctlMust("deploy", "--yes", "-t", "test")
 	assertSecretExists(t, k, p.TestSlug(), "secret")
-	assert.NotContains(t, stdout, "secret_value")
+	assert.NotContains(t, stdout, base64.StdEncoding.EncodeToString([]byte("secret_value")))
 
 	p.UpdateYaml("secret/secret-secret.yml", func(o *uo.UnstructuredObject) error {
 		_ = o.SetNestedField("secret_value_2", "stringData", "secret")
 		return nil
 	}, "")
 	stdout, _ = p.KluctlMust("deploy", "--yes", "-t", "test")
-	assert.NotContains(t, stdout, "secret_value")
+	assert.NotContains(t, stdout, base64.StdEncoding.EncodeToString([]byte("secret_value")))
 	assert.Contains(t, stdout, "***** (obfuscated)")
 
 	p.UpdateYaml("secret/secret-secret.yml", func(o *uo.UnstructuredObject) error {
@@ -45,4 +54,29 @@ func TestObfuscateSecrets(t *testing.T) {
 	assert.Contains(t, stdout, "-"+base64.StdEncoding.EncodeToString([]byte("secret_value_2")))
 	assert.Contains(t, stdout, "+"+base64.StdEncoding.EncodeToString([]byte("secret_value_3")))
 	assert.NotContains(t, stdout, "***** (obfuscated)")
+
+	// also test changing from empty data to filled data
+	p.UpdateYaml("secret2/secret-secret2.yml", func(o *uo.UnstructuredObject) error {
+		_ = o.SetNestedField(map[string]any{
+			"secret2": "secret_value_2",
+		}, "stringData")
+		return nil
+	}, "")
+
+	stdout, _ = p.KluctlMust("deploy", "--yes", "-t", "test")
+	assert.NotContains(t, stdout, base64.StdEncoding.EncodeToString([]byte("secret_value_2")))
+	assert.Contains(t, stdout, "+secret2: '***** (obfuscated)'")
+
+	p.UpdateYaml("secret3/secret-secret3.yml", func(o *uo.UnstructuredObject) error {
+		_ = o.SetNestedField(map[string]any{
+			"secret3": "secret_value_3",
+			"secret4": "secret_value_4",
+		}, "stringData")
+		return nil
+	}, "")
+	stdout, _ = p.KluctlMust("deploy", "--yes", "-t", "test")
+	assert.NotContains(t, stdout, base64.StdEncoding.EncodeToString([]byte("secret_value_3")))
+	assert.NotContains(t, stdout, base64.StdEncoding.EncodeToString([]byte("secret_value_4")))
+	assert.Contains(t, stdout, "+secret3: '***** (obfuscated)'")
+	assert.Contains(t, stdout, "+secret4: '***** (obfuscated)'")
 }
--- pkg/diff/obfuscate.go
+++ pkg/diff/obfuscate.go
@@ -19,6 +19,18 @@ func (o *Obfuscator) Obfuscate(ref k8s.ObjectRef, changes []types.Change) {
 }
 
 func (o *Obfuscator) obfuscateSecret(ref k8s.ObjectRef, changes []types.Change) {
+	setMapValues := func(m any, v string) {
+		if m == nil {
+			return
+		}
+		m2, ok := m.(map[string]any)
+		if ok {
+			for k, _ := range m2 {
+				m2[k] = v
+			}
+		}
+	}
+
 	for i, _ := range changes {
 		c := &changes[i]
 		if strings.HasPrefix(c.JsonPath, "data.") || strings.HasPrefix(c.JsonPath, "stringData.") {
@@ -37,6 +49,14 @@ func (o *Obfuscator) obfuscateSecret(ref k8s.ObjectRef, changes []types.Change) 
 				c.OldValue = "*****"
 				c.UnifiedDiff = strings.ReplaceAll(c.UnifiedDiff, "*****b", "***** (obfuscated)")
 			}
+		} else if c.JsonPath == "data" || c.JsonPath == "stringData" {
+			setMapValues(c.NewValue, "*****a")
+			setMapValues(c.OldValue, "*****b")
+			_ = updateUnifiedDiff(c)
+			c.UnifiedDiff = strings.ReplaceAll(c.UnifiedDiff, "*****a", "***** (obfuscated)")
+			c.UnifiedDiff = strings.ReplaceAll(c.UnifiedDiff, "*****b", "***** (obfuscated)")
+			setMapValues(c.NewValue, "*****")
+			setMapValues(c.OldValue, "*****")
 		}
 	}
 }
