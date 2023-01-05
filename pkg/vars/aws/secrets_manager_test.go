02ce5aeac13057a8564016b25ebaa95abc330c36
Aljoscha Poertner <aljoscha.poertner@hellmann.com>
2023-01-05 16:18:55 +0100

    test(vars): use assert pkg
    Signed-off-by: Aljoscha Poertner <aljoscha.poertner@hellmann.com>

--- pkg/vars/aws/secrets_manager_test.go
+++ pkg/vars/aws/secrets_manager_test.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -18,35 +19,22 @@ func TestGetAwsSecretsManagerSecret(t *testing.T) {
 
 	//Check if getting an AWS secret works
 	sc, err := GetAwsSecretsManagerSecret(context.TODO(), fcf, &tp, &tr, ts)
-	if err != nil {
-		t.Errorf("Getting an AWS secret failed")
-	}
-
-	if sc != tc {
-		t.Errorf("Content of secret is unexpected. Should be %s but is %s", tc, sc)
-	}
+	assert.NoError(t, err, "Getting an AWS secret failed")
+	assert.Equal(t, tc, sc, "Content of secret is unexpected.")
 
 	//Check if empty profile works
 	_, err = GetAwsSecretsManagerSecret(context.TODO(), fcf, nil, &tr, ts)
-	if err != nil {
-		t.Errorf("Getting an AWS secret with an empty profile throws an error: %s", err.Error())
-	}
+	assert.NoError(t, err, "Getting an AWS secret with an empty profile throws an error.")
 
 	//Check if empty region with non-ARN secret throws an error
 	_, err = GetAwsSecretsManagerSecret(context.TODO(), fcf, &tp, nil, ts)
-	if err == nil {
-		t.Errorf("Getting an AWS secret with an empty region and a non-ARN secret name should an error")
-	}
+	assert.Error(t, err, "Getting an AWS secret with an empty region and a non-ARN secret name should an error")
 
 	//Check if empty region with ARN secret works
 	_, err = GetAwsSecretsManagerSecret(context.TODO(), fcf, &tp, nil, completeArn)
-	if err != nil {
-		t.Errorf("Getting an AWS secret with an empty region and a ARN secret name throws an error: %s", err)
-	}
+	assert.NoError(t, err, "Getting an AWS secret with an empty region and a ARN secret name throws an error.")
 
 	//Check if wrong secret arn throws an error
 	_, err = GetAwsSecretsManagerSecret(context.TODO(), fcf, &tp, &tr, "")
-	if err == nil {
-		t.Errorf("Trying to get a secret with an empty name should throw an error")
-	}
+	assert.Error(t, err, "Trying to get a secret with an empty name should throw an error")
 }
