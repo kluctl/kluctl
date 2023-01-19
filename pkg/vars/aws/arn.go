ec62c3175ad8203b9373e6cb5e0faca0c1833b2c
Aljoscha Poertner <aljoscha.poertner@hellmann.com>
2023-01-19 17:11:01 +0100

    refactor(vars): extract splitting of resource to a separate method
    Signed-off-by: Aljoscha Poertner <aljoscha.poertner@posteo.de>

--- pkg/vars/aws/arn.go
+++ pkg/vars/aws/arn.go
@@ -1,42 +1,24 @@
 package aws
 
 import (
-	"fmt"
 	"strings"
 )
 
-type Arn struct {
-	Arn          string
-	Partition    string
-	Service      string
-	Region       string
-	Account      string
-	Resource     string
+type Resource struct {
+	ResourceId   string
 	ResourceType string
 }
 
-func ParseArn(arn string) (Arn, error) {
-	// http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
-	elements := strings.SplitN(arn, ":", 6)
-	if len(elements) < 6 {
-		return Arn{}, fmt.Errorf("%s is not a valid arn", arn)
-	}
-	var result Arn
-	result.Arn = elements[0]
-	result.Partition = elements[1]
-	result.Service = elements[2]
-	result.Region = elements[3]
-	result.Account = elements[4]
-	result.Resource = elements[5]
-
-	if strings.Index(result.Resource, "/") != -1 {
-		s := strings.SplitN(result.Resource, "/", 2)
+func SplitResource(resource string) (Resource, error) {
+	result := Resource{resource, ""}
+	if strings.Index(resource, "/") != -1 {
+		s := strings.SplitN(resource, "/", 2)
 		result.ResourceType = s[0]
-		result.Resource = s[1]
-	} else if strings.Index(result.Resource, ":") != -1 {
-		s := strings.SplitN(result.Resource, ":", 2)
+		result.ResourceId = s[1]
+	} else if strings.Index(resource, ":") != -1 {
+		s := strings.SplitN(resource, ":", 2)
 		result.ResourceType = s[0]
-		result.Resource = s[1]
+		result.ResourceId = s[1]
 	}
 	return result, nil
 }
