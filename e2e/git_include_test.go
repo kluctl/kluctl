8f98b33eaf96a80f0807b9de80801f8eb62486d6
Alexander Block <ablock84@gmail.com>
2023-02-03 23:00:45 +0100

    tests: Implement --local-git-group-override tests

--- e2e/git_include_test.go
+++ e2e/git_include_test.go
@@ -3,6 +3,7 @@ package e2e
 import (
 	"fmt"
 	test_utils "github.com/kluctl/kluctl/v2/e2e/test-utils"
+	git2 "github.com/kluctl/kluctl/v2/pkg/git"
 	git_url "github.com/kluctl/kluctl/v2/pkg/git/git-url"
 	"github.com/kluctl/kluctl/v2/pkg/utils/uo"
 	"github.com/kluctl/kluctl/v2/pkg/yaml"
@@ -12,8 +13,12 @@ import (
 	"testing"
 )
 
-func prepareIncludeProject(t *testing.T, prefix string, subDir string) *test_utils.TestProject {
-	p := test_utils.NewTestProject(t, test_utils.WithGitSubDir(subDir))
+func prepareIncludeProject(t *testing.T, prefix string, subDir string, gitServer *git2.TestGitServer) *test_utils.TestProject {
+	p := test_utils.NewTestProject(t,
+		test_utils.WithGitSubDir(subDir),
+		test_utils.WithGitServer(gitServer),
+		test_utils.WithRepoName(fmt.Sprintf("repos/%s", prefix)),
+	)
 	addConfigMapDeployment(p, "cm", map[string]string{"a": "v"}, resourceOpts{
 		name:      fmt.Sprintf("%s-cm", prefix),
 		namespace: p.TestSlug(),
@@ -21,12 +26,12 @@ func prepareIncludeProject(t *testing.T, prefix string, subDir string) *test_uti
 	return p
 }
 
-func prepareGitIncludeTest(t *testing.T) (*test_utils.TestProject, *test_utils.TestProject, *test_utils.TestProject) {
+func prepareGitIncludeTest(t *testing.T, gitServer *git2.TestGitServer) (*test_utils.TestProject, *test_utils.TestProject, *test_utils.TestProject) {
 	k := defaultCluster1
 
-	p := test_utils.NewTestProject(t)
-	ip1 := prepareIncludeProject(t, "include1", "")
-	ip2 := prepareIncludeProject(t, "include2", "subDir")
+	p := test_utils.NewTestProject(t, test_utils.WithGitServer(gitServer))
+	ip1 := prepareIncludeProject(t, "include1", "", gitServer)
+	ip2 := prepareIncludeProject(t, "include2", "subDir", gitServer)
 
 	createNamespace(t, k, p.TestSlug())
 
@@ -52,7 +57,7 @@ func TestGitInclude(t *testing.T) {
 
 	k := defaultCluster1
 
-	p, _, _ := prepareGitIncludeTest(t)
+	p, _, _ := prepareGitIncludeTest(t, nil)
 
 	p.KluctlMust("deploy", "--yes", "-t", "test")
 	assertConfigMapExists(t, k, p.TestSlug(), "include1-cm")
@@ -63,7 +68,7 @@ func TestLocalGitOverride(t *testing.T) {
 	t.Parallel()
 
 	k := defaultCluster1
-	p, ip1, ip2 := prepareGitIncludeTest(t)
+	p, ip1, ip2 := prepareGitIncludeTest(t, nil)
 
 	override1 := t.TempDir()
 	err := cp.Copy(ip1.LocalRepoDir(), override1)
@@ -91,6 +96,44 @@ func TestLocalGitOverride(t *testing.T) {
 	p.KluctlMust("deploy", "--yes", "-t", "test",
 		"--local-git-override", fmt.Sprintf("%s=%s", k1, override1),
 		"--local-git-override", fmt.Sprintf("%s=%s", k2, override2),
+	)
+	cm = assertConfigMapExists(t, k, p.TestSlug(), "include1-cm")
+	assertNestedFieldEquals(t, cm, "o1", "data", "a")
+	cm = assertConfigMapExists(t, k, p.TestSlug(), "include2-cm")
+	assertNestedFieldEquals(t, cm, "o2", "data", "a")
+}
+
+func TestLocalGitGroupOverride(t *testing.T) {
+	t.Parallel()
+
+	k := defaultCluster1
+	p, ip1, ip2 := prepareGitIncludeTest(t, git2.NewTestGitServer(t))
+
+	overrideGroupDir := t.TempDir()
+
+	override1 := filepath.Join(overrideGroupDir, "include1")
+	err := cp.Copy(ip1.LocalRepoDir(), override1)
+	assert.NoError(t, err)
+
+	override2 := filepath.Join(overrideGroupDir, "include2")
+	err = cp.Copy(ip2.LocalRepoDir(), override2)
+	assert.NoError(t, err)
+
+	cm, err := uo.FromFile(filepath.Join(override1, "cm", "configmap-include1-cm.yml"))
+	assert.NoError(t, err)
+	_ = cm.SetNestedField("o1", "data", "a")
+	_ = yaml.WriteYamlFile(filepath.Join(override1, "cm", "configmap-include1-cm.yml"), cm)
+
+	cm, err = uo.FromFile(filepath.Join(override2, "subDir", "cm", "configmap-include2-cm.yml"))
+	assert.NoError(t, err)
+	_ = cm.SetNestedField("o2", "data", "a")
+	_ = yaml.WriteYamlFile(filepath.Join(override2, "subDir", "cm", "configmap-include2-cm.yml"), cm)
+
+	u1, _ := git_url.Parse(p.GitServer().GitUrl() + "/repos")
+	k1 := u1.NormalizedRepoKey()
+
+	p.KluctlMust("deploy", "--yes", "-t", "test",
+		"--local-git-group-override", fmt.Sprintf("%s=%s", k1, overrideGroupDir),
 	)
 	cm = assertConfigMapExists(t, k, p.TestSlug(), "include1-cm")
 	assertNestedFieldEquals(t, cm, "o1", "data", "a")
