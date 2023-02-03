da34b2764dd01602a082816b4935365d8def2158
Alexander Block <ablock84@gmail.com>
2023-02-03 17:20:45 +0100

    tests: Implement --local-git-override tests

--- e2e/git_include_test.go
+++ e2e/git_include_test.go
@@ -3,7 +3,12 @@ package e2e
 import (
 	"fmt"
 	test_utils "github.com/kluctl/kluctl/v2/e2e/test-utils"
+	git_url "github.com/kluctl/kluctl/v2/pkg/git/git-url"
 	"github.com/kluctl/kluctl/v2/pkg/utils/uo"
+	"github.com/kluctl/kluctl/v2/pkg/yaml"
+	cp "github.com/otiai10/copy"
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
 	"testing"
 )
 
@@ -52,4 +57,41 @@ func TestGitInclude(t *testing.T) {
 	p.KluctlMust("deploy", "--yes", "-t", "test")
 	assertConfigMapExists(t, k, p.TestSlug(), "include1-cm")
 	assertConfigMapExists(t, k, p.TestSlug(), "include2-cm")
+}
+
+func TestLocalGitOverride(t *testing.T) {
+	t.Parallel()
+
+	k := defaultCluster1
+	p, ip1, ip2 := prepareGitIncludeTest(t)
+
+	override1 := t.TempDir()
+	err := cp.Copy(ip1.LocalRepoDir(), override1)
+	assert.NoError(t, err)
+
+	override2 := t.TempDir()
+	err = cp.Copy(ip2.LocalRepoDir(), override2)
+	assert.NoError(t, err)
+
+	cm, err := uo.FromFile(filepath.Join(override1, "cm", "configmap-include1-cm.yml"))
+	assert.NoError(t, err)
+	_ = cm.SetNestedField("o1", "data", "a")
+	_ = yaml.WriteYamlFile(filepath.Join(override1, "cm", "configmap-include1-cm.yml"), cm)
+
+	cm, err = uo.FromFile(filepath.Join(override2, "subDir", "cm", "configmap-include2-cm.yml"))
+	assert.NoError(t, err)
+	_ = cm.SetNestedField("o2", "data", "a")
+	_ = yaml.WriteYamlFile(filepath.Join(override2, "subDir", "cm", "configmap-include2-cm.yml"), cm)
+
+	u1, _ := git_url.Parse(ip1.GitUrl())
+	u2, _ := git_url.Parse(ip2.GitUrl())
+
+	p.KluctlMust("deploy", "--yes", "-t", "test",
+		"--local-git-override", fmt.Sprintf("%s=%s", u1.NormalizedRepoKey(), override1),
+		"--local-git-override", fmt.Sprintf("%s=%s", u2.NormalizedRepoKey(), override2),
+	)
+	cm = assertConfigMapExists(t, k, p.TestSlug(), "include1-cm")
+	assertNestedFieldEquals(t, cm, "o1", "data", "a")
+	cm = assertConfigMapExists(t, k, p.TestSlug(), "include2-cm")
+	assertNestedFieldEquals(t, cm, "o2", "data", "a")
 }
