b6be3dd265170a2ad1c66921908507b77f2d0017
Alexander Block <ablock84@gmail.com>
2023-03-22 15:43:23 +0100

    refactor: Remove unused WriteYamlToTar

--- pkg/yaml/yaml.go
+++ pkg/yaml/yaml.go
@@ -1,7 +1,6 @@
 package yaml
 
 import (
-	"archive/tar"
 	"bytes"
 	"encoding/json"
 	"errors"
@@ -176,27 +175,6 @@ func WriteYamlAllStream(w io.Writer, l []interface{}) error {
 		if err != nil {
 			return err
 		}
-	}
-	return nil
-}
-
-func WriteYamlToTar(tw *tar.Writer, o interface{}, name string) error {
-	str, err := WriteYamlBytes(o)
-	if err != nil {
-		return err
-	}
-
-	err = tw.WriteHeader(&tar.Header{
-		Name: name,
-		Size: int64(len(str)),
-		Mode: 0o666 | tar.TypeReg,
-	})
-	if err != nil {
-		return err
-	}
-	_, err = tw.Write(str)
-	if err != nil {
-		return err
 	}
 	return nil
 }
