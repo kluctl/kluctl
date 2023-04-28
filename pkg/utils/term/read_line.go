6f355698164f76d30e4c570642cc0a1e28edd4fd
Alexander Block <ablock84@gmail.com>
2023-04-28 16:36:03 +0200

    fix: Ignore ESC in readLine (#439)

--- pkg/utils/term/read_line.go
+++ pkg/utils/term/read_line.go
@@ -17,6 +17,9 @@ func readLine(reader io.Reader, cb func(ret []byte)) ([]byte, error) {
 		n, err := reader.Read(buf[:])
 		if n > 0 {
 			switch buf[0] {
+			case '':
+				// ignore ESC sequences
+				continue
 			case '', '\b':
 				if len(ret) > 0 {
 					ret = ret[:len(ret)-1]
