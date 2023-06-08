12662156a12448467d0eacacf4c712e571e2200f
Alexander Block <ablock84@gmail.com>
2023-06-08 22:20:06 +0200

    refactor: Remove unused TargetSummary/ProjectSummary

--- pkg/types/result/summary.go
+++ pkg/types/result/summary.go
@@ -2,7 +2,6 @@ package result
 
 import (
 	"github.com/kluctl/kluctl/v2/pkg/types"
-	"sort"
 )
 
 type CommandResultSummary struct {
@@ -28,19 +27,6 @@ type CommandResultSummary struct {
 	Warnings []DeploymentError `json:"warnings"`
 
 	TotalChanges int `json:"totalChanges"`
-}
-
-type TargetSummary struct {
-	Target TargetKey `json:"target"`
-
-	LastValidateResult *ValidateResult        `json:"lastValidateResult,omitempty"`
-	CommandResults     []CommandResultSummary `json:"commandResults,omitempty"`
-}
-
-type ProjectSummary struct {
-	Project ProjectKey `json:"project"`
-
-	Targets []*TargetSummary `json:"targets"`
 }
 
 func (cr *CommandResult) BuildSummary() *CommandResultSummary {
@@ -80,46 +66,5 @@ func (cr *CommandResult) BuildSummary() *CommandResultSummary {
 	for _, o := range cr.Objects {
 		ret.TotalChanges += len(o.Changes)
 	}
-	return ret
-}
-
-func BuildProjectSummaries(summaries []CommandResultSummary) []*ProjectSummary {
-	m := map[ProjectKey]*ProjectSummary{}
-	for _, rs := range summaries {
-		p, ok := m[rs.ProjectKey]
-		if !ok {
-			p = &ProjectSummary{Project: rs.ProjectKey}
-			m[rs.ProjectKey] = p
-		}
-
-		var target *TargetSummary
-		for i, t := range p.Targets {
-			if t.Target == rs.TargetKey {
-				target = p.Targets[i]
-				break
-			}
-		}
-		if target == nil {
-			target = &TargetSummary{
-				Target: rs.TargetKey,
-			}
-			p.Targets = append(p.Targets, target)
-		}
-
-		target.CommandResults = append(target.CommandResults, rs)
-	}
-
-	ret := make([]*ProjectSummary, 0, len(m))
-	for _, p := range m {
-		sort.Slice(p.Targets, func(i, j int) bool {
-			return p.Targets[i].Target.Less(p.Targets[j].Target)
-		})
-		ret = append(ret, p)
-	}
-
-	sort.Slice(ret, func(i, j int) bool {
-		return ret[i].Project.Less(ret[j].Project)
-	})
-
 	return ret
 }
