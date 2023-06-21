636469345cf6c08b8f8ccb49dc997858aee76267
Alexander Block <ablock84@gmail.com>
2023-06-21 09:10:49 +0200

    ci: Remove npm build folder from git

--- pkg/webui/ui/build.js
+++
@@ -1,7 +0,0 @@
-const rewire = require('rewire');
-const defaults = rewire('react-scripts/scripts/build.js');
-const config = defaults.__get__('config');
-
-// we disable minimize as we're actually committing the build folder into git, so that people who don't want to deal
-// with the UI while working on Go code don't have to use npm and friends
-config.optimization.minimize = false
--- pkg/webui/ui/build/staticbuild.js
+++
@@ -1,1 +0,0 @@
-const staticResults = new Map()
