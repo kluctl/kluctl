caf322531fe2b738fb3d17267c837bf3b6db8f26
Alexander Block <ablock84@gmail.com>
2023-08-09 17:12:18 +0200

    refactor: Move websocket accepting into helper function

--- pkg/webui/events.go
+++ pkg/webui/events.go
@@ -12,7 +12,6 @@ import (
 	"github.com/kluctl/kluctl/v2/pkg/yaml"
 	"net/http"
 	"nhooyr.io/websocket"
-	"strings"
 	"sync"
 	"time"
 )
@@ -252,18 +251,7 @@ func (h *eventsHandler) handler(c *gin.Context) {
 		}
 	}
 
-	acceptOptions := &websocket.AcceptOptions{
-		InsecureSkipVerify: true,
-	}
-
-	userAgentLower := strings.ToLower(c.GetHeader("User-Agent"))
-	isSafari := strings.Contains(userAgentLower, "safari") && !strings.Contains(userAgentLower, "chrome") && !strings.Contains(userAgentLower, "android")
-
-	if isSafari {
-		acceptOptions.CompressionMode = websocket.CompressionDisabled
-	}
-
-	conn, err := websocket.Accept(c.Writer, c.Request, acceptOptions)
+	conn, err := acceptWebsocket(c)
 	if err != nil {
 		return
 	}
---
+++ pkg/webui/utils.go
@@ -0,0 +1,27 @@
+package webui
+
+import (
+	"github.com/gin-gonic/gin"
+	"nhooyr.io/websocket"
+	"strings"
+)
+
+func acceptWebsocket(ctx *gin.Context) (*websocket.Conn, error) {
+	acceptOptions := &websocket.AcceptOptions{
+		InsecureSkipVerify: true,
+	}
+
+	userAgentLower := strings.ToLower(ctx.GetHeader("User-Agent"))
+	isSafari := strings.Contains(userAgentLower, "safari") && !strings.Contains(userAgentLower, "chrome") && !strings.Contains(userAgentLower, "android")
+
+	if isSafari {
+		acceptOptions.CompressionMode = websocket.CompressionDisabled
+	}
+
+	conn, err := websocket.Accept(ctx.Writer, ctx.Request, acceptOptions)
+	if err != nil {
+		return nil, nil
+	}
+
+	return conn, err
+}
