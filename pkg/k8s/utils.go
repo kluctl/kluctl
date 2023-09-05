e2db6af2147ddc26aabf28396c9fa68dfd1f825f
Alexander Block <ablock84@gmail.com>
2023-09-05 16:37:38 +0200

    feat: Auto-generate static credentials and auth secret if non-existent

--- pkg/k8s/utils.go
+++ pkg/k8s/utils.go
@@ -6,7 +6,9 @@ import (
 	"github.com/kluctl/kluctl/v2/pkg/types/k8s"
 	"github.com/kluctl/kluctl/v2/pkg/utils/uo"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/rand"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -68,16 +70,48 @@ func GetClusterId(ctx context.Context, c client.Client) (string, error) {
 }
 
 func GetSingleSecret(ctx context.Context, c client.Client, name string, namespace string, key string) (string, error) {
+	return doGetOrGenerateSingleSecret(ctx, c, name, namespace, key, false, "")
+}
+
+func GetOrGenerateSingleSecret(ctx context.Context, c client.Client, name string, namespace string, key string, manager string) (string, error) {
+	return doGetOrGenerateSingleSecret(ctx, c, name, namespace, key, true, manager)
+}
+
+func doGetOrGenerateSingleSecret(ctx context.Context, c client.Client, name string, namespace string, key string, allowGenerate bool, manager string) (string, error) {
 	var secret corev1.Secret
 	err := c.Get(ctx, client.ObjectKey{Name: name, Namespace: namespace}, &secret)
+	if err != nil {
+		if !allowGenerate || !errors.IsNotFound(err) {
+			return "", err
+		}
+		secret.Name = name
+		secret.Namespace = namespace
+		err = c.Create(ctx, &secret, client.FieldOwner(manager))
+		if err != nil && !errors.IsAlreadyExists(err) {
+			return "", err
+		}
+	}
+
+	if secret.Data == nil {
+		secret.Data = map[string][]byte{}
+	}
+
+	x, ok := secret.Data[key]
+	if ok {
+		return string(x), nil
+	}
+	if !allowGenerate {
+		return "", fmt.Errorf("secrets %s has no key %s", name, key)
+	}
+
+	generated := rand.String(32)
+	patch := client.MergeFrom(secret.DeepCopy())
+	secret.Data[key] = []byte(generated)
+
+	err = c.Patch(ctx, &secret, patch, client.FieldOwner(manager))
 	if err != nil {
 		return "", err
 	}
 
-	x, ok := secret.Data[key]
-	if !ok {
-		return "", fmt.Errorf("secret %s has no '%s' key", name, key)
-	}
-
-	return string(x), nil
+	return generated, nil
 }
