a42c684d9d9d9f464a1ef986f658c6e977e7bf66
Alexander Block <ablock84@gmail.com>
2023-09-05 12:06:56 +0200

    feat: Allow to retry manual deployments via the rocket button

--- pkg/webui/ui/src/components/target-view/ManualApproveButton.tsx
+++ pkg/webui/ui/src/components/target-view/ManualApproveButton.tsx
@@ -3,49 +3,61 @@ import { useAppContext } from "../App";
 import React from "react";
 import { RocketLaunch } from "@mui/icons-material";
 import { Box, IconButton, Tooltip } from "@mui/material";
+import { buildReconcilingState } from "./ReconcilingIcon";
 
 export const ManualApproveButton = (props: {ts: TargetSummary, renderedObjectsHash: string}) => {
     const appCtx = useAppContext()
-    const handleApprove = (approve: boolean) => {
-        if (!props.ts.kdInfo || !props.ts.kd) {
-            return
-        }
-        if (approve) {
-            if (!props.renderedObjectsHash) {
-                return
-            }
-            appCtx.api.setManualObjectsHash(props.ts.kdInfo.clusterId, props.ts.kdInfo.name, props.ts.kdInfo.namespace, props.renderedObjectsHash)
-        } else {
-            appCtx.api.setManualObjectsHash(props.ts.kdInfo.clusterId, props.ts.kdInfo.name, props.ts.kdInfo.namespace, "")
-        }
-    }
 
     if (appCtx.isStatic || !appCtx.user.isAdmin || props.ts.kd?.deployment.spec.dryRun || !props.ts.kd?.deployment.spec.manual) {
         return <></>
     }
 
-    const hasDrift = !!props.ts.lastDriftDetectionResult?.objects?.length
-    const isApproved = props.ts.kd.deployment.spec.manualObjectsHash === props.renderedObjectsHash
+    const state = buildReconcilingState(props.ts)
+    if (!state) {
+        return <></>
+    }
+
+    const isApproved = state?.manualObjectsHash === props.renderedObjectsHash
+
+    const handleApproveButton = () => {
+        if (!props.ts.kdInfo || !props.ts.kd) {
+            return
+        }
+        if (!props.renderedObjectsHash) {
+            return
+        }
+        if (!state.hasDrift) {
+            return
+        }
+        if (!isApproved) {
+            appCtx.api.setManualObjectsHash(props.ts.kdInfo.clusterId, props.ts.kdInfo.name, props.ts.kdInfo.namespace, props.renderedObjectsHash)
+        } else {
+            appCtx.api.deployNow(props.ts.kdInfo.clusterId, props.ts.kdInfo.name, props.ts.kdInfo.namespace)
+        }
+    }
 
     let icon: React.ReactElement
     let tooltip: string
-    if (!hasDrift) {
+    if (!state.hasDrift) {
         tooltip = "No drift, so there is nothing to be manually deployed!"
         icon = <RocketLaunch color={"disabled"}/>
     } else if (!isApproved) {
         tooltip = "Click here to trigger this manual deployment."
         icon = <RocketLaunch color={"info"}/>
     } else {
-        tooltip = "Click here to cancel this deployment. This will only have an effect if the deployment has not started reconciliation yet!"
-        icon = <RocketLaunch color={"success"}/>
+        if (state.requestedDeploy) {
+            tooltip = "Deployment has been requested, please wait!"
+            icon = <RocketLaunch color={"disabled"}/>
+        } else {
+            tooltip = "Click here to retry this already approved manual deployment."
+            icon = <RocketLaunch color={"warning"}/>
+        }
     }
     return <Box display='flex' gap='6px' alignItems='center' height='39px'>
         <IconButton
             onClick={e => {
                 e.stopPropagation();
-                if (hasDrift) {
-                    handleApprove(!isApproved)
-                }
+                handleApproveButton()
             }}
             sx={{
                 padding: 0,
--- pkg/webui/ui/src/components/target-view/ReconcilingIcon.tsx
+++ pkg/webui/ui/src/components/target-view/ReconcilingIcon.tsx
@@ -6,67 +6,113 @@ import { Done, Error, Hotel, HourglassTop, SyncProblem } from "@mui/icons-materi
 import { MessageQuestionIcon } from "../../icons/Icons";
 import Tooltip from "@mui/material/Tooltip";
 
+export interface Condition {
+    status: string
+    reason: string
+    message: string
+    lastTransitionTime: string
+}
+
+export interface ReconcilingState {
+    suspended: boolean
+    requestedReconcile: string
+    requestedValidate: string
+    requestedDeploy: string
+
+    statusExists: boolean
+    readyCondition?: Condition
+    reconcilingCondition?: Condition
+
+    manualObjectsHash?: string
+    hasDrift: boolean
+    lastDriftDetectionResult?: any
+    lastDriftDetectionResultMessage: string
+}
+
+export function buildReconcilingState(ts: TargetSummary): ReconcilingState | undefined {
+    if (!ts.kd) {
+        return undefined
+    }
+    const annotations: any = ts.kd.deployment.metadata?.annotations || {}
+    const requestReconcile = annotations["kluctl.io/request-reconcile"]
+    const requestValidate = annotations["kluctl.io/request-validate"]
+    const requestDeploy = annotations["kluctl.io/request-deploy"]
+
+    const conditions: any[] | undefined = ts.kd.deployment.status.conditions
+    const readyCondition = conditions?.find(c => c.type === "Ready")
+    const reconcilingCondition = conditions?.find(c => c.type === "Reconciling")
+
+    const lastDriftDetectionResult: any | undefined = ts.kd.deployment.status?.lastDriftDetectionResult
+
+    return {
+        suspended: ts.kd.deployment.spec.suspend,
+        requestedReconcile: requestReconcile && requestReconcile !== ts.kd.deployment.status?.lastHandledReconcileAt && requestReconcile,
+        requestedValidate: requestValidate && requestValidate !== ts.kd.deployment.status?.lastHandledValidateAt && requestValidate,
+        requestedDeploy: requestDeploy && requestDeploy !== ts.kd.deployment.status?.lastHandledDeployAt && requestDeploy,
+        statusExists: !!ts.kd.deployment.status,
+        readyCondition: readyCondition,
+        reconcilingCondition: reconcilingCondition,
+        manualObjectsHash: ts.kd.deployment.spec.manualObjectsHash,
+        hasDrift: !!lastDriftDetectionResult?.objects?.length,
+        lastDriftDetectionResult: lastDriftDetectionResult,
+        lastDriftDetectionResultMessage: ts.kd.deployment.status?.lastDriftDetectionResultMessage,
+    }
+}
+
 export const ReconcilingIcon = (props: { ps: ProjectSummary, ts: TargetSummary }) => {
     const theme = useTheme();
 
-    if (!props.ts.kd) {
+    const state = buildReconcilingState(props.ts)
+
+    if (!state) {
         return <></>
     }
 
-    const buildStateTime = (c: any) => {
+    const buildStateTime = (c: Condition) => {
         return <>In this state since <Since startTime={c.lastTransitionTime}/></>
     }
 
     let icon: React.ReactElement | undefined = undefined
     const tooltip: React.ReactNode[] = []
 
-    const annotations: any = props.ts.kd.deployment.metadata?.annotations || {}
-    const requestReconcile = annotations["kluctl.io/request-reconcile"]
-    const requestValidate = annotations["kluctl.io/request-validate"]
-    const requestDeploy = annotations["kluctl.io/request-deploy"]
-
-    if (props.ts.kd.deployment.spec.suspend) {
+    if (state.suspended) {
         icon = <Hotel color={"primary"}/>
         tooltip.push("Deployment is suspended")
-    } else if (!props.ts.kd.deployment.status) {
+    } else if (!state.statusExists) {
         icon = <MessageQuestionIcon color={theme.palette.warning.main}/>
         tooltip.push("Status not available")
     } else {
-        const conditions: any[] | undefined = props.ts.kd.deployment.status.conditions
-        const readyCondition = conditions?.find(c => c.type === "Ready")
-        const reconcilingCondition = conditions?.find(c => c.type === "Reconciling")
-
-        if (reconcilingCondition) {
+        if (state.reconcilingCondition) {
             icon = <CircularProgress color={"info"} size={24}/>
-            tooltip.push(reconcilingCondition.message)
-            tooltip.push(buildStateTime(reconcilingCondition))
+            tooltip.push(state.reconcilingCondition.message)
+            tooltip.push(buildStateTime(state.reconcilingCondition))
         } else {
-            if (requestReconcile && requestReconcile !== props.ts.kd.deployment.status.lastHandledReconcileAt) {
+            if (state.requestedReconcile) {
                 icon = <HourglassTop color={"primary"}/>
-                tooltip.push("Reconcile requested: " + requestReconcile)
-            } else if (requestValidate && requestValidate !== props.ts.kd.deployment.status.lastHandledValidateAt) {
+                tooltip.push("Reconcile requested: " + state.requestedReconcile)
+            } else if (state.requestedValidate) {
                 icon = <HourglassTop color={"primary"}/>
-                tooltip.push("Validate requested: " + requestValidate)
-            } else if (requestDeploy && requestDeploy !== props.ts.kd.deployment.status.lastHandledDeployAt) {
+                tooltip.push("Validate requested: " + state.requestedValidate)
+            } else if (state.requestedDeploy) {
                 icon = <HourglassTop color={"primary"}/>
-                tooltip.push("Deploy requested: " + requestReconcile)
-            } else if (readyCondition) {
-                if (readyCondition.status === "True") {
+                tooltip.push("Deploy requested: " + state.requestedDeploy)
+            } else if (state.readyCondition) {
+                if (state.readyCondition.status === "True") {
                     icon = <Done color={"success"}/>
-                    tooltip.push(readyCondition.message)
+                    tooltip.push(state.readyCondition.message)
                 } else {
-                    if (readyCondition.reason === "PrepareFailed") {
+                    if (state.readyCondition.reason === "PrepareFailed") {
                         icon = <SyncProblem color={"error"}/>
-                        tooltip.push(readyCondition.message)
-                    } else if (readyCondition.reason === "ValidateFailed") {
+                        tooltip.push(state.readyCondition.message)
+                    } else if (state.readyCondition.reason === "ValidateFailed") {
                         icon = <Done color={"success"}/>
-                        tooltip.push(readyCondition.message)
+                        tooltip.push(state.readyCondition.message)
                     } else {
                         icon = <Error color={"warning"}/>
-                        tooltip.push(readyCondition.message)
+                        tooltip.push(state.readyCondition.message)
                     }
                 }
-                tooltip.push(buildStateTime(readyCondition))
+                tooltip.push(buildStateTime(state.readyCondition))
             } else {
                 icon = <MessageQuestionIcon color={theme.palette.warning.main}/>
                 tooltip.push("Ready condition is missing")
