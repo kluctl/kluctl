ab9ef316f449e9ce998cb5ef960b861ec0c06134
Alexander Block <ablock84@gmail.com>
2023-09-27 10:46:57 +0200

    refacotor: Introduce GetK8sAnnotationBoolPtr

--- pkg/utils/uo/k8s_fields.go
+++ pkg/utils/uo/k8s_fields.go
@@ -204,16 +204,27 @@ func (uo *UnstructuredObject) GetK8sAnnotationsWithRegex(r interface{}) map[stri
 	return ret
 }
 
-func (uo *UnstructuredObject) GetK8sAnnotationBool(name string, defaultValue bool) (bool, error) {
+func (uo *UnstructuredObject) GetK8sAnnotationBoolPtr(name string) (*bool, error) {
 	s := uo.GetK8sAnnotation(name)
 	if s == nil {
-		return defaultValue, nil
+		return nil, nil
 	}
 	b, err := strconv.ParseBool(*s)
 	if err != nil {
-		return defaultValue, fmt.Errorf("failed to parse annotation %s=%v as bool", name, *s)
+		return nil, fmt.Errorf("failed to parse annotation %s=%v as bool", name, *s)
 	}
-	return b, nil
+	return &b, nil
+}
+
+func (uo *UnstructuredObject) GetK8sAnnotationBool(name string, defaultValue bool) (bool, error) {
+	b, err := uo.GetK8sAnnotationBoolPtr(name)
+	if err != nil {
+		return defaultValue, err
+	}
+	if b == nil {
+		return defaultValue, nil
+	}
+	return *b, nil
 }
 
 func (uo *UnstructuredObject) GetK8sAnnotationBoolNoError(name string, defaultValue bool) bool {
