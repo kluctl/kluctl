8b1684285d0d205b698416865939baf659b38cbe
Alexander Block <ablock84@gmail.com>
2024-02-28 11:51:49 +0100

    tests: Add TestOnlyOneNamespacePermissions

--- e2e/remote_objects_permission_test.go
+++ e2e/remote_objects_permission_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/kluctl/kluctl/v2/e2e/test_project"
 	"github.com/stretchr/testify/assert"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"strings"
 	"testing"
 )
 
@@ -131,4 +132,99 @@ roleRef:
 
 	p.KluctlMust(t, "deploy", "--yes", "-t", "test", "--write-command-result=false")
 	assertConfigMapExists(t, k, p.TestSlug(), "cm2")
+}
+
+func TestOnlyOneNamespacePermissions(t *testing.T) {
+	t.Parallel()
+
+	k := defaultCluster1
+
+	p := test_project.NewTestProject(t)
+
+	username := p.TestSlug()
+	au, err := k.AddUser(envtest.User{Name: username}, nil)
+	assert.NoError(t, err)
+
+	createNamespace(t, k, p.TestSlug())
+
+	p.UpdateTarget("test", nil)
+
+	addConfigMapDeployment(p, "cm", nil, resourceOpts{
+		name:      "cm1",
+		namespace: p.TestSlug(),
+	})
+
+	rbac := strings.NewReplacer(
+		"USERNAME", username,
+		"SLUG", p.TestSlug(),
+	).Replace(`
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: USERNAME
+rules:
+  - apiGroups: [""]
+    resources: ["namespaces"]
+    resourceNames: ["SLUG"]
+    verbs: ["create", "update", "patch", "get", "list", "watch"]
+  - apiGroups: ["rbac.authorization.k8s.io"]
+    resources: ["*"]
+    verbs: ["create", "update",  "patch", "get", "list", "watch"]
+---
+kind: Role
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: USERNAME
+  namespace: SLUG
+rules:
+  - apiGroups: [""]
+    resources: ["configmaps"]
+    verbs: ["create", "update", "patch", "get", "list", "watch"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: USERNAME
+subjects:
+  - kind: User
+    name: USERNAME
+roleRef:
+  kind: ClusterRole
+  name: USERNAME
+  apiGroup: rbac.authorization.k8s.io
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: USERNAME
+  namespace: SLUG
+subjects:
+  - kind: User
+    name: USERNAME
+roleRef:
+  kind: Role
+  name: USERNAME
+  apiGroup: rbac.authorization.k8s.io
+`)
+	p.AddKustomizeDeployment("rbac", []test_project.KustomizeResource{
+		{Name: "rbac.yaml", Content: rbac},
+	}, nil)
+
+	p.KluctlMust(t, "deploy", "--yes", "-t", "test")
+	assertConfigMapExists(t, k, p.TestSlug(), "cm1")
+
+	kc, err := au.KubeConfig()
+	assert.NoError(t, err)
+
+	p.AddExtraArgs("--kubeconfig", getKubeconfigTmpFile(t, kc))
+
+	addConfigMapDeployment(p, "cm2", nil, resourceOpts{
+		name:      "cm2",
+		namespace: p.TestSlug(),
+	})
+
+	_, stderr, err := p.Kluctl(t, "deploy", "--yes", "-t", "test")
+	assert.NoError(t, err)
+	assertConfigMapExists(t, k, p.TestSlug(), "cm2")
+	assert.Contains(t, stderr, "Not enough permissions to write to the result store.")
 }
