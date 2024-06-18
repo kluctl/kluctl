a58135f2d5e4a3d2e9f9fb00c402a7e9a9d50eb8
Alexander Block <ablock84@gmail.com>
2024-06-18 11:24:04 +0200

    fix: Don't fail when the git repo has no commits

--- pkg/git/git_info.go
+++ pkg/git/git_info.go
@@ -2,7 +2,9 @@ package git
 
 import (
 	"context"
+	"errors"
 	git2 "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/kluctl/kluctl/v2/pkg/types"
 	"github.com/kluctl/kluctl/v2/pkg/types/result"
 	"os"
@@ -42,11 +44,6 @@ func BuildGitInfo(ctx context.Context, repoRoot string, projectDir string) (resu
 		return gitInfo, projectKey, err
 	}
 
-	head, err := g.Head()
-	if err != nil {
-		return gitInfo, projectKey, err
-	}
-
 	remotes, err := g.Remotes()
 	if err != nil {
 		return gitInfo, projectKey, err
@@ -62,27 +59,32 @@ func BuildGitInfo(ctx context.Context, repoRoot string, projectDir string) (resu
 		}
 	}
 
-	var repoKey types.RepoKey
-	if originUrl != nil {
-		repoKey = originUrl.RepoKey()
-	}
-
 	gitInfo = result.GitInfo{
 		Url:    originUrl,
 		SubDir: subDir,
-		Commit: head.Hash().String(),
 		Dirty:  !s.IsClean(),
 	}
-	if head.Name().IsBranch() {
-		gitInfo.Ref = &types.GitRef{
-			Branch: head.Name().Short(),
+
+	head, err := g.Head()
+	if err == nil {
+		gitInfo.Commit = head.Hash().String()
+		if head.Name().IsBranch() {
+			gitInfo.Ref = &types.GitRef{
+				Branch: head.Name().Short(),
+			}
+		} else if head.Name().IsTag() {
+			gitInfo.Ref = &types.GitRef{
+				Tag: head.Name().Short(),
+			}
 		}
-	} else if head.Name().IsTag() {
-		gitInfo.Ref = &types.GitRef{
-			Tag: head.Name().Short(),
-		}
+	} else if !errors.Is(err, plumbing.ErrReferenceNotFound) {
+		return gitInfo, projectKey, err
 	}
-	projectKey.RepoKey = repoKey
+
+	if originUrl != nil {
+		projectKey.RepoKey = originUrl.RepoKey()
+	}
 	projectKey.SubDir = subDir
+
 	return gitInfo, projectKey, nil
 }
