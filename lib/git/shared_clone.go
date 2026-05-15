ad2da77b4221010db090cfb14282ddaf12d12ccd
Alexander Block <ablock84@gmail.com>
2026-05-15 12:54:38 +0200

    feat: Use shared clone instead of custom poor mans clone

---
+++ lib/git/shared_clone.go
@@ -0,0 +1,58 @@
+package git
+
+import (
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/storage/filesystem"
+)
+
+func FastSharedClone(sourceDir string, targetDir string, coOptions *git.CheckoutOptions) error {
+	fs := osfs.New(targetDir, osfs.WithBoundOS())
+	gitFs, err := fs.Chroot(".git")
+	if err != nil {
+		return err
+	}
+	s := filesystem.NewStorageWithOptions(gitFs, cache.NewObjectLRUDefault(), filesystem.Options{})
+	defer s.Close()
+
+	r, err := git.Clone(s, fs, &git.CloneOptions{
+		URL:        sourceDir,
+		Shared:     true,
+		NoCheckout: true,
+	})
+	if err != nil {
+		return err
+	}
+	err = r.DeleteRemote("origin")
+	if err != nil {
+		return err
+	}
+
+	sourceRepo, err := git.PlainOpen(sourceDir)
+	if err != nil {
+		return err
+	}
+	remotes, err := sourceRepo.Remotes()
+	if err != nil {
+		return err
+	}
+	for _, remote := range remotes {
+		_, err = r.CreateRemote(remote.Config())
+		if err != nil {
+			return err
+		}
+	}
+
+	wt, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	err = wt.Checkout(coOptions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
